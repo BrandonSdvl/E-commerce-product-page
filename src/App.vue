5f6a5bf032a58d087370ea9a8544c0a924b35525
BrandonSandoval46 <brandonsandoval194@gmail.com>
2024-07-29 15:42:47 -0600

    Updating dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,40 @@
+<script setup>
+// import Header from "./components/Header.vue";
+// import Product from "./components/Product.vue";
+// import Modal from "./components/Modal.vue";
+// import Images from "./components/Images.vue";
+// import Footer from "./components/Footer.vue";
+import { ref } from 'vue'
+
+const selected = ref(0);
+
+const show = (el, name) => {
+  if (!el.classList.contains(`${name}--show`)) {
+    el.classList.add(`${name}--show`);
+  } else {
+    el.classList.remove(`${name}--show`);
+  }
+}
+
+</script>
+
 <template lang="pug">
 #app
-  Header
-  Product(:selected="selected")
-  Modal
-    Images(:images="products[selected].images", :modal="true")
-  Footer
+  //- Header
+  //- Product(:selected="selected")
+  //- Modal
+  //-   Images(:images="products[selected].images", :modal="true")
+  //- Footer
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import Product from "./components/Product.vue";
-import Modal from "./components/Modal.vue";
-import Images from "./components/Images.vue";
-import Footer from "./components/Footer.vue";
-import { mapState } from "vuex";
 
-export default {
-  name: "App",
-  components: {
-    Header,
-    Product,
-    Modal,
-    Images,
-    Footer,
-  },
-  computed: {
-    ...mapState(["products"]),
-  },
-  data() {
-    return {
-      selected: 0,
-    };
-  },
-  methods: {
-    show(el, name) {
-      if (!el.classList.contains(`${name}--show`)) {
-        el.classList.add(`${name}--show`);
-      } else {
-        el.classList.remove(`${name}--show`);
-      }
-    },
-  },
-};
+
+// export default {
+  // computed: {
+  //   ...mapState(["products"]),
+  // },
+// };
 </script>
 
 <style lang="scss" src="./scss/styles.scss"></style>
