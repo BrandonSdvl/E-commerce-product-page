3171fad90a8a56811065da46e2dcdb1b930c25ac
BrandonSandoval46 <brandonsandoval194@gmail.com>
2024-08-06 21:31:52 -0600

    Fixing Carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,24 +1,31 @@
 <script setup>
 import IconNext from "../assets/icon-next.svg?component";
 import IconPrevious from "../assets/icon-previous.svg?component";
-import { ref, computed, onMounted, onBeforeUnmount } from 'vue'
+import { ref, computed, onMounted, onBeforeUnmount } from 'vue';
 import { useSlideStore } from '@/store';
+import { getImageUrl } from '@/utils/imageUtils';
 
 const slideStore = useSlideStore();
 
-const xInitial = ref(0)
-const itemWidth = ref(0)
-const img = ref(new Image())
-const carouselList = ref(null)
+const xInitial = ref(0);
+const itemWidth = ref(0);
+const img = ref(new Image());
+const carouselList = ref(null);
 
 const props = defineProps({
-  images: Array,
-  modal: Boolean,
-})
+  images: {
+    type: Array,
+    required: true,
+  },
+  modal: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 onMounted(() => {
   window.addEventListener("resize", handleResize);
-  itemWidth.value = carouselList.value.firstElementChild.clientWidth;
+  itemWidth.value = carouselList.value.clientWidth;
   img.value.src = new URL('../assets/transparent.png', import.meta.url).href;
 });
 
@@ -28,25 +35,18 @@ onBeforeUnmount(() => {
 
 const slidesLen = computed(() => {
   return props.images.length;
-})
-
-const drop = (e) => {
-  e.preventDefault();
-}
+});
 
 const startTouch = (e) => {
   xInitial.value = e.changedTouches[0].clientX;
-}
+};
 
 const endTouch = (e) => {
-  if (Math.abs(e.changedTouches[0].clientX - xInitial) >= 50) {
-    if (e.changedTouches[0].clientX - xInitial > 0) {
-      prev();
-    } else if (e.changedTouches[0].clientX - xInitial < 0) {
-      next();
-    }
+  const deltaX = e.changedTouches[0].clientX - xInitial.value;
+  if (Math.abs(deltaX) >= 50) {
+    deltaX > 0 ? prev() : next();
   }
-}
+};
 
 const startDrag = (e, id) => {
   e.dataTransfer.dropEffect = "move";
@@ -54,101 +54,80 @@ const startDrag = (e, id) => {
   e.dataTransfer.setData("itemID", id);
   e.dataTransfer.setDragImage(img.value, 1, 1);
   xInitial.value = e.x;
-}
+};
 
 const endDrag = (e) => {
-  if (Math.abs(e.x - xInitial) >= 50) {
-    if (e.x - xInitial > 0) {
-      prev();
-    } else if (e.x - xInitial < 0) {
-      next();
-    }
+  const deltaX = e.clientX - xInitial.value;
+  if (Math.abs(deltaX) >= 50) {
+    deltaX > 0 ? prev() : next();
   }
-}
+};
 
 const prev = () => {
-  if (slideStore.visibleSlide <= 0) {
-    slideStore.visibleSlide = slidesLen.value - 1;
-  } else {
-    slideStore.visibleSlide--;
-  }
+  slideStore.setVisibleSlide(slideStore.visibleSlide <= 0 ? slidesLen.value - 1 : slideStore.visibleSlide - 1)
   adjustCarousel();
-}
+};
 
 const next = () => {
-  if (slideStore.visibleSlide >= slidesLen.value - 1) {
-    slideStore.visibleSlide = 0;
-  } else {
-    slideStore.visibleSlide++;
-  }
+  slideStore.setVisibleSlide(slideStore.visibleSlide >= slidesLen.value - 1 ? 0 : slideStore.visibleSlide + 1);
   adjustCarousel();
-}
+};
 
 const changeImage = (idx) => {
-  if (slideStore.visibleSlide === idx) {
-    return;
-  } else {
+  if (slideStore.visibleSlide !== idx) {
     slideStore.visibleSlide = idx;
     adjustCarousel();
   }
-}
+};
 
 const handleModal = () => {
   const modal = document.getElementById("modal");
-  modal.classList.toggle('modal--show')
-}
-
-const handleResize = () => {
-  itemWidth.value = carouselList.value.firstElementChild.clientWidth;
-  adjustCarousel();
-}
-
-const adjustCarousel = () => {
-  carouselList.value.style.transform = `translateX(-${itemWidth.value * slideStore.visibleSlide
-    }px)`;
-}
-
-const imgImport = import.meta.glob('/src/assets/*.jpg', { eager: true });
-
-const getImageUrl = (imageName) => {
-  const image = imgImport[`/src/assets/${imageName}`];
-  return image ? image.default : '';
+  modal?.classList.toggle('modal--show');
 };
 
+const handleResize = () => {
+  itemWidth.value = carouselList.value.clientWidth;
+  adjustCarousel();
+};
+
+const adjustCarousel = () => {
+  slideStore.setTranslateX(itemWidth.value * slideStore.visibleSlide);
+};
 
 defineExpose({
-  changeImage
-})
+  changeImage,
+});
 
 </script>
 
 <template lang="pug">
 .carousel
   .carousel__btn.carousel__btn--prev(
-    @click="prev()",
+    @click="prev",
     :class="props.modal ? 'modal__btn' : 'carousel__btn'"
   )
     IconPrevious
   .carousel__list-container
     .carousel__list(
       ref="carouselList",
-      @drop="drop",
+      :style="{ transform: `translateX(-${slideStore.translateX}px)` }",
+      @drop.prevent,
       @dragenter.prevent,
       @dragover.prevent
     )
       div(v-for="(i, idx) in props.images", :key="idx")
         img.carousel__item(
           :src="getImageUrl(i)",
-          @click="!props.modal ? handleModal() : ''",
+          @click="!props.modal ? handleModal() : null",
           alt="Product image",
           draggable="true",
           @dragstart="startDrag($event, idx)",
-          @dragend="endDrag($event)",
-          @touchstart="startTouch($event)",
-          @touchend="endTouch($event)"
+          @dragend="endDrag",
+          @touchstart="startTouch",
+          @touchend="endTouch"
         )
   .carousel__btn.carousel__btn--next(
-    @click="next()",
+    @click="next",
     :class="props.modal ? 'modal__btn' : 'carousel__btn'"
   )
     IconNext
--- src/store/slideStore.js
+++ src/store/slideStore.js
@@ -3,6 +3,14 @@ import { defineStore } from 'pinia';
 export const useSlideStore = defineStore('slide', {
   state: () => ({
     visibleSlide: 0,
-    direction: "left",
+    translateX: 0,
   }),
+  actions: {
+    setVisibleSlide(slide) {
+      this.visibleSlide = slide;
+    },
+    setTranslateX(x) {
+      this.translateX = x;
+    },
+  },
 });
